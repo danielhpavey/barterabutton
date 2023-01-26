306ee0903d99fa9611c95520e8e7a47124e9521c
Daniel <danielhpavey@gmail.com>
2023-01-26 15:18:40 +0000

    Content Updates

--- pages/index.js
+++ pages/index.js
@@ -21,27 +21,38 @@ export default function Home() {
       </header>
       <main className="mt-4 px-8 max-w-2xl mx-auto">
         
-        <p>On August 31st 2022 I embarked upon quite a strange yet interesting adventure.</p>
+      <p>Hi I&#39;m Sharon and on August 30th 2022 I embarked upon quite a strange yet interesting adventure.</p>
+<p>I decided to offer a blue plastic button I had no use for on a local zero waste Facebook group in Exeter UK where I live. I was asking if anyone would like to offer me something they no longer needed in return for the button which they could make some use of....</p>
+<img src="/button-min.png" alt="Barter a Button" className="my-8 mx-auto block"/>
+<p>Read on for the rest of this story (so far). If you need my contact details to message me, they&#39;re at the bottom of this page. </p>
+<p>So back to the story....</p>
+<p>Hannah replied to my post on Facebook and offered me a stapler for the button, then Becka told me she really needed a stapler but had a violin she no longer had any use for. I was inundated with people wanting the violin which was surprising as I would have thought it was quite a niche object. Off it went to it’s new home with two musicians and I had a pretty fancy Bean to Cup coffee machine in it’s place.</p>
+<img src="/stapler-min.png" alt="Barter a Button Stapler" className="my-8 mx-auto block"/>
+<p>The coffee machine also proved very popular and yet again, I had a lot of offers, including several cash offers which often led to me having to explain that ‘I simply cannot take cash for the item as I’m doing a cash-free challenge’.</p>
+<p>The coffee machine found it’s new home with an artist who responded to a complimentary comment I put on one of her lovely paintings on social media. I had come across Alice Mary having seen her incredible wall murals upstairs in the Exeter Guildhall Shopping Centre.</p>
+<img src="/cathedral-painting-min.png" alt="Barter a Button Cathederal Painting" className="my-8 mx-auto block"/>
+<p>Alice offered to create an original painting just for my challenge in exchange for the coffee machine and I went up to Bristol to collect the most beautiful painting of Exeter Cathedral. </p>
+<p>Ruud offered me a holiday at his beautiful Mazzard Farm holiday cottages in East Devon in exchange for the painting. I was starting to attract a few followers on social media and they were very excited about a holiday. I even involved them in the decisions asking them what I should choose for the next swap. </p>
+<p>The holiday was handed over to Leigh Farmer and her lovely family to enjoy. Leigh is a photographer based in Exeter specialising in company images. </p>
+<p>An artist in Totnes called Anita Nowinska was interested in the photo shoot as she needed her marketing materials updating on her website. We made the swap live on BBC Radio Devon which was pretty nerve-wracking but exciting. Anita offered a large painting of whatever the next person would like her to paint for them. We then went on to be featured on local West Country TV on BBC Spotlight. </p>
+<img src="/anita-min.png" alt="Barter a Button Anita Painting" className="my-8 mx-auto block"/>
 
-        <p>I decided to offer a blue plastic button I had no use for on a local zero waste Facebook group. I was asking if anyone would like to offer me something they no longer needed in return for the button which they could make some use of.</p>
+<p>Item number 9</p>
+<p>And this is where we are up to now. I do have item number 9 lined up and I&#39;m making the swap very soon. The best place to keep up to date is Instagram <a href="https://www.instagram.com/barterabutton">instagram.com/barterabutton</a></p>
+<p>I should explain – this is not my idea. This concept is called the Red Paperclip Challenge which was started by Kyle Macdonald back in 2006 in Canada. Kyle took just over a year and swapped items (from a red paperclip) all the way up to house. This was replicated last year by American Demi Skipper who also reached a house. </p>
+<p>I know some people have tried it here in the UK but I don’t think anyone has succeeded in reaching a house yet – but that is my dream !</p>
+<p>We are in the situation like many families in the UK where saving for an enormous deposit for our own house is virtually impossible – whilst raising a family and paying rent. </p>
+<p>Doing this challenge requires a set of skills which seem to match mine – and it seems to be working so far! </p>
+<p>I love that it’s cash-free as it just shows that not everything has to involve money and it really makes you think about the worth of things and what one person thinks is worth a lot, another does not. Bartering has been around for thousands of years so really it’s nothing new!</p>
+<p>If you would like to contact me my email is <a href="mailto:sharon@barterabutton.com">sharon@barterabutton.com</a></p>
+<p>I do most of my updates on Instagram &amp; Facebook. See <a href="https://www.instagram.com/barterabutton">instagram.com/barterabutton</a> </p>
 
-        <p>Hannah replied and offered me a stapler for the button, then Becka told me she really needed a stapler but had a violin she no longer had any use for.  I was inundated with people wanting the violin which was surprising as I would have thought it was quite a niche object. Off it went to it’s new home with two musicians and I had a pretty fancy Bean to Cup coffee machine in it’s place.</p>
-
-        <p>The coffee machine also proved very popular and yet again, I had a lot of offers, including several cash offers which often led to me having to explain that ‘I simply cannot take cash for the item as I’m doing a cash-free challenge’.</p>
-
-        <p>The coffee machine has found it’s new home now with an artist who responded to a complimentary comment I put on one of her lovely paintings on social media. I had come across Alice Mary having seen her incredible wall murals upstairs in the Exeter Guildhall Shopping Centre.</p>
-
-        <p>Alice offered to create an original painting in exchange for the coffee machine and this is where we are up to in the challenge. I should explain – it is not my idea. This is called the Red Paperclip Challenge which was started by Kyle Macdonald back in 2006 in Canada. Kyle took just over a year and swapped items (from a red paperclip) all the way up to house. This was replicated last year by American Demi Skipper – who took 18 months and swapped 28 items to also make it to a house. I know some people have tried here in the UK but I don’t think anyone has succeeded in reaching a house yet – but that is my dream !</p>
-
-        <p>We are in the situation like many families in the UK where saving for an enormous deposit for our own house is virtually impossible – whilst raising a family and paying rent. Doing this challenge requires a set of skills which seem to match my experience – and it seems to be working so far! I love that it’s cash-free as it just shows that not everything has to involve money and it really makes you think about the worth of things and what one person thinks is worth a lot,  another does not – this especially applies to art and I’m really thrilled to have a piece of art as part of this journey. Bartering has been around for thousands of years so really it’s nothing new!</p>
-
-        <p>So onto the painting – as I’m hoping you are reading this as you’ve heard about what I’m doing and might want to make me an offer. The painting is an original unique image of Exeter Cathedral on canvas. It’s 80x60cm large and is embossed with gold leaf.</p>
       </main>
 
     <div className="border-button-dark border-y-2 mb-12 my-4">
       <footer className="flex flex-row justify-between flex-wrap gap-4 px-8 my-4  max-w-2xl mx-auto ">
           <div>
-            <a href="https://instagram.com/barterabutton">My Instagram</a>
+            <a href="https://instagram.com/barterabutton">Instagram</a>
           </div>
           <div>
             <a href="mailto:sharon@barterabutton.com">sharon@barterabutton.com</a>
